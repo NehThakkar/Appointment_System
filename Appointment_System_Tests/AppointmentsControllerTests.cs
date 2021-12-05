81f84d05a4f11cf067a5205cdf6ec44a1f97264c
Neh Rakeshkumar Thakkar <200458205@student.georgianc.on.ca>
2021-12-05 16:29:58 -0500

    Some simplicity changes and added comments

--- Appointment_System_Tests/AppointmentsControllerTests.cs
+++ Appointment_System_Tests/AppointmentsControllerTests.cs
@@ -14,7 +14,7 @@ namespace Appointment_System_Tests
         private ApplicationDbContext _context;
         AppointmentsController controller;
 
-
+        // Initialize Mock Data to Use in Tests
         [TestInitialize]
         public void TestInitialize()
         {
@@ -23,6 +23,7 @@ namespace Appointment_System_Tests
                 .Options;
             _context = new ApplicationDbContext(options);
 
+            // new admin object
             var admin = new Admin
             {
                 AdminId = 1000,
@@ -32,7 +33,8 @@ namespace Appointment_System_Tests
             };
             _context.Admins.Add(admin);
 
-            var appointment1 = new Appointment
+            // new appointment objects
+            var appointment = new Appointment
             {
                 AppointmentID = 1000,
                 FirstName = "First Name",
@@ -47,9 +49,9 @@ namespace Appointment_System_Tests
                 Initials = admin.Initials,
                 AdminId = admin.AdminId
             };
-            _context.Appointments.Add(appointment1);
+            _context.Appointments.Add(appointment);
 
-            var appointment2 = new Appointment
+            appointment = new Appointment
             {
                 AppointmentID = 1001,
                 FirstName = "First Name",
@@ -64,16 +66,20 @@ namespace Appointment_System_Tests
                 Initials = admin.Initials,
                 AdminId = admin.AdminId
             };
-            _context.Appointments.Add(appointment2);
+            _context.Appointments.Add(appointment);
 
+            // saving _context after adding an admin and two apoointments
             _context.SaveChanges();
 
             controller = new AppointmentsController(_context);
 
         }
 
+
+        // this region has a few tests for DELETE (GET) in Apoointments Controller
         #region Delete_Get
 
+        // This Test Method checks wether the Enetered parameter Appointment Id is null then it loads 404
         [TestMethod]
         public void DeleteNullIdLoads404()
         {
@@ -81,6 +87,7 @@ namespace Appointment_System_Tests
             Assert.AreEqual("404",result.ViewName);
         }
 
+        // This Test Method Checks if the entered Appointment Id is not valid then it loads 404
         [TestMethod]
         public void DeleteInvalidIdLoads404()
         {
@@ -88,6 +95,7 @@ namespace Appointment_System_Tests
             Assert.AreEqual("404", result.ViewName);
         }
 
+        // This Test Method Checks if the entered Appointment Id is valid then it loads Delete View
         [TestMethod]
         public void DeleteValidIdDeletesAppointment()
         {
@@ -95,6 +103,7 @@ namespace Appointment_System_Tests
             Assert.AreEqual("Delete", result.ViewName);
         }
 
+        // This Test Method Checks if the entered Appointment Id is valid then it targets that Exact Apoointment
         [TestMethod]
         public void ValidIdRetrievesExactAppointment()
         {
@@ -102,6 +111,7 @@ namespace Appointment_System_Tests
             Assert.AreEqual(_context.Appointments.Find(1001),result.Model);
         }
 
+        // This Test Method Checks if the entered Appointment Id is valid then it doesn't target any other Appointment
         [TestMethod]
         public void ValidIdsButNotExactApoointments()
         {
